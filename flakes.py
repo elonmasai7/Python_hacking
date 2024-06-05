548fcafd9232e1ef14c72fa0b1faa89d4e221bc6
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-06-05 20:32:06 +0300

    about

--- flakes.py
+++ flakes.py
@@ -1,20 +1,19 @@
 import subprocess
-import os
 import shlex
 
 def run_bandit(sanitized_path):
     command = ['/usr/local/bin/bandit', '-r', sanitized_path]
-    result = subprocess.run(command, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
     print(result.stdout)
 
 def run_flake8(sanitized_path):
     command = ['/usr/local/bin/flake8', sanitized_path]
-    result = subprocess.run(command, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
     print(result.stdout)
 
 def run_pylint(sanitized_path):
     command = ['/usr/local/bin/pylint', sanitized_path]
-    result = subprocess.run(command, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
     print(result.stdout)
 
 # Example usage
