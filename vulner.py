1e3c472ebd3a7aab3a7900ccd7db731212e21ce6
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-06-08 19:49:02 +0300

    fkf

--- vulner.py
+++ vulner.py
@@ -10,7 +10,17 @@ def run_flake8(sanitized_path):
     command = ['/usr/local/bin/flake8', sanitized_path]
     result = subprocess.run(command, capture_output=True, text=True, check=True)
     print(result.stdout)
+command = ['/usr/local/bin/bandit', '-r', sanitized_path]
+result = subprocess.run(command, capture_output=True, text=True, check=True)
+print(result.stdout)
 
+command = ['/usr/local/bin/flake8', sanitized_path]
+result = subprocess.run(command, capture_output=True, text=True, check=True)
+print(result.stdout)
+
+command = ['/usr/local/bin/pylint', sanitized_path]
+result = subprocess.run(command, capture_output=True, text=True, check=True)
+print(result.stdout)
 def run_pylint(sanitized_path):
     command = ['/usr/local/bin/pylint', sanitized_path]
     result = subprocess.run(command, capture_output=True, text=True, check=True)
