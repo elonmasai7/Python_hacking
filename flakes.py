51ab5c73aa754b40c9631ea9a90790e2fb309819
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-06-09 21:30:03 +0300

    Elon

--- flakes.py
+++ flakes.py
@@ -1,7 +1,7 @@
 import subprocess
 import shlex
 
-# Example of sanitizing inputs before using in subprocess.run
+
 def run_bandit(sanitized_path):
     command = ['/usr/local/bin/bandit', '-r', sanitized_path]
     result = subprocess.run(command, capture_output=True, text=True, check=True)
