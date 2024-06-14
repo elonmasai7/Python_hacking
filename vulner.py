059c55b28e79118e7f278bfc4f68e5f52378d04c
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-06-14 22:00:51 +0300

    updated

--- vulner.py
+++ vulner.py
@@ -1,17 +1,16 @@
-import subprocess
 import os
 
 def run_bandit(path):
     print(f"Running Bandit on {path}")
-    result = subprocess.run(['/usr/local/bin/bandit', '-r', path], capture_output=True, text=True)
+    result = subprocess.run(['/usr/local/bin/bandit', '-r', path], capture_output=True, text=True, shell=False)
     print(result.stdout)
 
 def run_flake8(path):
     print(f"Running Flake8 on {path}")
-    result = subprocess.run(['/usr/local/bin/flake8', path], capture_output=True, text=True)
+    result = subprocess.run(['/usr/local/bin/flake8', path], capture_output=True, text=True, shell=False)
     print(result.stdout)
 
 def run_pylint(path):
     print(f"Running Pylint on {path}")
-    result = subprocess.run(['/usr/local/bin/pylint', path], capture_output=True, text=True)
+    result = subprocess.run(['/usr/local/bin/pylint', path], capture_output=True, text=True, shell=False)
     print(result.stdout)
